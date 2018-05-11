aa27781fc659689ec7d65005493835a497992363
Ruby <rubylichtenstein@citi.com>
2018-05-11 16:52:51 +0300

    test to spek

--- src/test/kotlin/com/ornoyman/kovfefe/KovfefeTest.kt
+++ src/test/kotlin/com/ornoyman/kovfefe/KovfefeTest.kt
@@ -1,26 +1,28 @@
 package com.ornoyman.kovfefe
 
-import org.junit.Test
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 
-enum class Pokemon { Squirtle, Bulbasaur, Charmander }
+object CreateMatcherTest : Spek({
+    describe("Generate data class") {
+        on("Data class with basic types") {
+            data class BasicType(
+                val string: String,
+                val int: Int,
+                val long: Long,
+                val float: Float,
+                val boolean: Boolean,
+                val double: Double,
+                val char: Char,
+                val byte: Byte
+            )
 
-
-data class BasicType(
-    val string: String,
-    val int: Int,
-    val long: Long,
-    val float: Float,
-    val boolean: Boolean,
-    val double: Double,
-    val char: Char,
-    val byte: Byte
-)
-
-class KovfefeTest {
-
-    @Test
-    fun testBasicType() {
-        val foo = generate<BasicType>()
-        println(foo)
+            it("Should generate") {
+                val foo = generate<BasicType>()
+                println(foo)
+            }
+        }
     }
-}
+})
