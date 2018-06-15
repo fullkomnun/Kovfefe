0b390ecfe13d3796049cb63fcbb7fe128811fea7
rl98880 <ruby.lichtenstein@citi.com>
2018-06-15 21:03:14 +0300

    data class gen test

--- src/test/kotlin/com/ornoyman/kovfefe/KovfefeDataClassSpecs.kt
+++ src/test/kotlin/com/ornoyman/kovfefe/KovfefeDataClassSpecs.kt
@@ -1,60 +1,43 @@
 package com.ornoyman.kovfefe
 
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
-object KovfefeDataClassSpecs: Spek({
-    //    describe("Generate data class") {
-//        data class BasicType(
-//            val string: String,
-//            val int: Int,
-//            val long: Long,
-//            val float: Float,
-//            val boolean: Boolean,
-//            val double: Double,
-//            val char: Char,
-//            val byte: Byte
-//        )
-//
-//        on("Data class with basic types") {
-//            xit("Should generate") {
-//                val foo = generate<BasicType>()
-//                assertTrue { !foo.boolean }
-//                assertTrue { foo.string.isEmpty() }
-//                assertTrue { foo.int == 0 }
-//                assertTrue { foo.long == 0L }
-//                assertTrue { foo.float == 0.0f }
-//                assertTrue { foo.double == 0.0 }
-//                assertTrue { foo.char == 'a' }
-//                println(foo)
-//            }
-//
-//            xit("Should generate with default value") {
-//                val foo = generate<BasicType>()
-//                println(foo)
-//            }
-//
-//            xit("Kotlin default value") {
-//                data class BasicTypeDefaults(
-//                    val string: String = "kovfefe",
-//                    val int: Int = 42,
-//                    val long: Long = 42L,
-//                    val float: Float = 42.5f,
-//                    val boolean: Boolean = true,
-//                    val double: Double = 42.5,
-//                    val char: Char = 'b',
-//                    val byte: Byte = Byte.MIN_VALUE
-//                )
-//
-//                val foo = generate<BasicTypeDefaults>()
-//                assertTrue { foo.boolean }
-//                assertTrue { foo.string == "kovfefe" }
-//                assertTrue { foo.int == 42 }
-//                assertTrue { foo.long == 42L }
-//                assertTrue { foo.float == 42.5f }
-//                assertTrue { foo.double == 42.5 }
-//                assertTrue { foo.char == 'b' }
-//                println(foo)
-//            }
-//        }
-//    }
+object KovfefeDataClassSpecs : Spek({
+    describe("Generate data class") {
+        data class BasicType(val string: String)
+
+        on("Data class") {
+            it("Should generate ") {
+                val generated = generate<BasicType>()
+                assertNotNull(generated)
+            }
+
+            it("Should generate with default value") {
+                data class BasicTypeDefaults(
+                    val string: String = "kovfefe",
+                    val int: Int = 42,
+                    val long: Long = 42L,
+                    val float: Float = 42.5f,
+                    val boolean: Boolean = true,
+                    val double: Double = 42.5,
+                    val char: Char = 'b',
+                    val byte: Byte = Byte.MIN_VALUE
+                )
+
+                val generated = generate<BasicTypeDefaults>()
+                assertTrue { generated.boolean }
+                assertTrue { generated.string == "kovfefe" }
+                assertTrue { generated.int == 42 }
+                assertTrue { generated.long == 42L }
+                assertTrue { generated.float == 42.5f }
+                assertTrue { generated.double == 42.5 }
+                assertTrue { generated.char == 'b' }
+            }
+        }
+    }
 })
