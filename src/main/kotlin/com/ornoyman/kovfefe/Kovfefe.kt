4dc70304760e66d564ac6dbaf8bce411f69cd7a7
ornoyman <or.noyman@citi.com>
2018-06-15 17:08:49 +0300

    added big numbers + more collection types

--- src/main/kotlin/com/ornoyman/kovfefe/Kovfefe.kt
+++ src/main/kotlin/com/ornoyman/kovfefe/Kovfefe.kt
@@ -2,6 +2,7 @@ package com.ornoyman.kovfefe
 
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
+import kotlin.reflect.KParameter
 import kotlin.reflect.full.primaryConstructor
 
 inline fun <reified T : Any> generate(): T = T::class.generate()
@@ -11,27 +12,22 @@ fun <T : Any> KClass<T>.generate(): T = when {
     isTopLevelAutoValue -> generateTopLevelAutoValue()
     isNestedAutoValue -> generateNestedAutoValue()
     java.isEnum -> java.enumConstants.first()
-    else -> genFields() as T
+    basicTypes.containsKey(this) -> basicTypes[this] as T
+    bigNumberTypes.containsKey(this) -> bigNumberTypes[this] as T
+    collectionTypes.containsKey(this) -> collectionTypes[this] as T
+    else -> throw IllegalArgumentException("Cannot generate $qualifiedName")
 }
 
 private fun <T : Any> KClass<T>.generateDataClass(): T = primaryConstructor!!.invoke()
 
-internal fun <T : Any> KFunction<T>.invoke(): T {
-    val params = parameters
+internal fun <T : Any> KFunction<T>.invoke(): T = callBy(generateParameters())
+
+private fun <T : Any> KFunction<T>.generateParameters(): Map<KParameter, Any?> =
+    parameters
         .filter { !it.isOptional }
         .filter { it.type.classifier is KClass<*> }
-        .associate {
-            it to if (it.type.isMarkedNullable) null
-            else (it.type.classifier as KClass<*>).generate()
-        }
-    return callBy(params)
-}
+        .associate { it to generateParameter(it) }
 
-private fun <T : Any> KClass<T>.genFields(): Any? {
-    return when {
-        collection.containsKey(qualifiedName) -> collection[qualifiedName]
-        basicType.containsKey(qualifiedName) -> basicType[qualifiedName]
-        else -> throw IllegalArgumentException("Cannot generate $qualifiedName")
-    }
-}
-
+private fun generateParameter(it: KParameter) =
+    if (it.type.isMarkedNullable) null
+    else (it.type.classifier as KClass<*>).generate()
--- src/main/kotlin/com/ornoyman/kovfefe/Types.kt
+++ src/main/kotlin/com/ornoyman/kovfefe/Types.kt
@@ -1,66 +1,36 @@
 package com.ornoyman.kovfefe
 
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.util.*
 
-internal val collection = hashMapOf(
-    List::class.qualifiedName to listOf<Any>(),
-    Set::class.qualifiedName to setOf<Any>(),
-    Map::class.qualifiedName to mapOf<Any, Any>(),
-    HashMap::class.qualifiedName to hashMapOf<Any, Any>(),
-    LinkedHashSet::class.qualifiedName to linkedSetOf<Any>(),
-    ArrayList::class.qualifiedName to arrayListOf<Any>(),
-    LinkedHashMap::class.qualifiedName to linkedMapOf<Any, Any>()
+internal val basicTypes = hashMapOf(
+    String::class to "",
+    Int::class to 0,
+    Long::class to 0L,
+    Float::class to 0.0f,
+    Boolean::class to false,
+    Double::class to 0.0,
+    Char::class to 'a',
+    Byte::class to Byte.MAX_VALUE
 )
 
-internal val kotlinType = hashMapOf(
-    Pair::class.qualifiedName to Pair<Any, Any>(Unit, Unit),
-    Triple::class.qualifiedName to Triple<Any, Any, Any>(Unit, Unit, Unit)
+internal val bigNumberTypes = hashMapOf(
+    BigInteger::class to BigInteger.ZERO,
+    BigDecimal::class to BigDecimal.ZERO
 )
 
-internal val basicType = hashMapOf(
-    String::class.qualifiedName to "",
-    Int::class.qualifiedName to 0,
-    Long::class.qualifiedName to 0L,
-    Float::class.qualifiedName to 0.0f,
-    Boolean::class.qualifiedName to false,
-    Double::class.qualifiedName to 0.0,
-    Char::class.qualifiedName to 'a',
-    Byte::class.qualifiedName to Byte.MAX_VALUE
+internal val collectionTypes = hashMapOf(
+    List::class to listOf<Any>(),
+    MutableList::class to mutableListOf<Any>(),
+    ArrayList::class to arrayListOf<Any>(),
+    LinkedList::class to LinkedList<Any>(),
+    Set::class to setOf<Any>(),
+    MutableSet::class to mutableSetOf<Any>(),
+    LinkedHashSet::class to linkedSetOf<Any>(),
+    TreeSet::class to sortedSetOf<Any>(),
+    Map::class to mapOf<Any, Any>(),
+    MutableMap::class to mutableMapOf<Any, Any>(),
+    HashMap::class to hashMapOf<Any, Any>(),
+    LinkedHashMap::class to linkedMapOf<Any, Any>()
 )
-
-/*
-*
-* private fun forClassName(className: String): Any =
-    when (className) {
-        "org.threeten.bp.LocalDate" -> LocalDate.now()
-        "org.threeten.bp.LocalDateTime" -> LocalDateTime.now()
-        "org.threeten.bp.LocalTime" -> LocalTime.now()
-        "org.threeten.bp.ZonedDateTime" -> ZonedDateTime.now()
-        "java.math.BigInteger" -> BigInteger.ZERO
-        "java.math.BigDecimal" -> BigDecimal.ZERO
-        "java.lang.String" -> ""
-        "kotlin.String" -> ""
-        "java.lang.Integer" -> 0
-        "kotlin.Int" -> 0
-        "java.lang.Long" -> 0L
-        "kotlin.Long" -> 0L
-        "java.lang.Boolean" -> false
-        "kotlin.Boolean" -> false
-        "java.lang.Float" -> 0.0f
-        "kotlin.Float" -> 0.0f
-        "java.lang.Double" -> 0.0
-        "kotlin.Double" -> 0.0
-        "java.lang.Character" -> 'a'
-        "kotlin.Char" -> 'a'
-        "java.lang.Byte" -> Byte.MAX_VALUE
-        "kotlin.Byte" -> Byte.MAX_VALUE
-        else -> throw IllegalArgumentException("Cannot generate $className")
-    }*/
-/*    "java.lang.String" -> ""
-"java.lang.Integer" -> 0
-"java.lang.Long" -> 0L
-"java.lang.Boolean" -> false
-"java.lang.Float" -> 0.0f
-"java.lang.Double" -> 0.0
-"java.lang.Character" -> 'a'
-"java.lang.Byte" -> Byte.MAX_VALUE
-*/
